a03c0da2892118106be5b1054c67549b6f00ce1b
kai <thebiggestkai@gmail.com>
2023-10-31 21:29:42 -0700

    Implement gui and listener

---
+++ listener.py
@@ -0,0 +1,17 @@
+import pyautogui
+from pynput.mouse import Listener
+
+# Mouse event handler
+def on_click(x, y, button, pressed):
+    if pressed:
+        action = 'Pressed'
+    else:
+        action = 'Released'
+    print(f'Mouse {action} at ({x}, {y}) with button {button}')
+
+# Create a mouse listener
+with Listener(on_click=on_click) as listener:
+    listener.join()
+
+
+
