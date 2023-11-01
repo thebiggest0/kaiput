888a341a9f21b53c2313e00de48c88d1cd9ad126
Kai <thebiggestkai@gmail.com>
2023-10-31 22:06:25 -0700

    Add mouse & keyboard listener

--- listener.py
+++ listener.py
@@ -1,16 +1,25 @@
-from pynput.mouse import Listener
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Listener as KeyboardListener
 
-# Mouse event handler
+#Mouse event handler
 def on_click(x, y, button, pressed):
-    if pressed:
-        action = 'Pressed'
-    else:
-        action = 'Released'
+    action = 'Pressed' if pressed else 'Released'
     print(f'Mouse {action} at ({x}, {y}) with button {button}')
 
-# Create a mouse listener
-with Listener(on_click=on_click) as listener:
-    listener.join()
+#Keyboard event handler
+def on_key_event(key):
+    try:
+        print(f'Key {key.char} {key.name} was pressed')
+    except AttributeError:
+        print(f'Special key {key} was pressed')
 
+#Create mouse listener
+mouse_listener = MouseListener(on_click=on_click)
 
+#Create keyboard listener
+keyboard_listener = KeyboardListener(on_press=on_key_event)
 
+#Start both listeners
+with mouse_listener, keyboard_listener:
+    mouse_listener.join()
+    keyboard_listener.join()
